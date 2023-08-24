7c4a6410f2ff6fbcd37aca5ec08c040e67155da8
Marcos Bento <Marcos.Bento@ecmwf.int>
2023-08-24 10:01:51 +0100

    Use ecKit CURL facilities
    n.b. This considers a version of ecKit that offers an implementation of HTTP PUT
    
    Re ECFLOW-1892

--- src/libecflow_light/ecflow/light/TinyREST.cc
+++ src/libecflow_light/ecflow/light/TinyREST.cc
@@ -8,12 +8,9 @@
  * nor does it submit to any jurisdiction.
  */
 
-#include <curl/curl.h>
 
-#include <stdexcept>
-
+#include <eckit/io/EasyCURL.h>
 #include <eckit/utils/StringTools.h>
-#include <eckit/utils/Tokenizer.h>
 
 #include "ecflow/light/TinyREST.h"
 
@@ -44,50 +41,7 @@ std::vector<Status> Status::status_set_ = {
     // Server Error responses
     Status{Code::INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR"}};
 
-namespace WrapperCURL {
-
-/**
- * Copies the content from input `userdata` into output `buffer`, returning the number of characters copied
- */
-size_t read_callback(char* buffer, size_t size [[maybe_unused]], size_t nitems [[maybe_unused]], void* userdata) {
-    auto data = static_cast<std::string*>(userdata);
-
-    memcpy(buffer, data->c_str(), data->size());
-    return data->size();
-}
-
-size_t write_headers_callback(char* buffer, size_t size, size_t nitems, void* userdata) {
-    auto data = static_cast<Fields*>(userdata);
-
-    std::string all = eckit::StringTools::trim(std::string(buffer, size * nitems), " \n\r");
-
-    // Ignore 'response-line'
-    if (eckit::StringTools::startsWith(all, "HTTP/")) {
-        return size * nitems;
-    }
-
-    // Ignore (final) empty line
-    if (all.empty()) {
-        return size * nitems;
-    }
-
-    std::vector<std::string> tokens;
-    eckit::Tokenizer tokenizer(":");
-    tokenizer(all, tokens);
-
-    data->insert(tokens[0], eckit::StringTools::trim(tokens[1]));
-
-    return size * nitems;
-}
-
-size_t write_body_callback(char* buffer, size_t size, size_t nitems, void* userdata) {
-    auto data = static_cast<Body*>(userdata);
-
-    std::string all = eckit::StringTools::trim(std::string(buffer, size * nitems), " \n\r");
-    *data = Body(all);
-
-    return size * nitems;
-}
+namespace detail {
 
 class URL {
 public:
@@ -103,222 +57,117 @@ private:
     Target target_;
 };
 
-struct UnsuccessfulOperation : public std::runtime_error {
-    explicit UnsuccessfulOperation(const char* what) : std::runtime_error(what) {}
-
-    template <typename... ARGS>
-    explicit UnsuccessfulOperation(ARGS... args) : std::runtime_error(stringify(args...).c_str()) {}
-};
-
-class Handle;
-
-class List {
-public:
-    List() : values_{nullptr} {}
-    ~List() { curl_slist_free_all(values_); }
-
-    void append(const std::string& name, const std::string& value) {
-        std::string header = stringify(name, ": ", value);
-        values_            = curl_slist_append(values_, header.c_str());
-    }
-
-private:
-    friend Handle;
-    [[nodiscard]] struct curl_slist* native() const { return values_; }
-
-    struct curl_slist* values_;
-};
-
 class Handle {
 public:
-    static Handle make_handle(const URL& url, const WrapperCURL::List& fields) {
-        Handle curl;
-
-        curl.set_noprogress();
-        curl.set_keepalive();
-        curl.set_maxredirs(50L);
-
+    Handle() : handle_{} {
         // TODO: Remove the following insecurities!
-        curl.set_verifyhost(false);
-        curl.set_verifypeer(false);
-        curl.set_verifystatus(false);
-
-        curl.set_url(url);
-
-        curl.set_headers(fields);
-
-        return curl;
+        set_verifyhost(false);
+        set_verifypeer(false);
     }
 
-    Handle() : handle_{curl_easy_init()} {
-        if (!handle_) {
-            throw UnsuccessfulOperation("Unable to initialise cURL handle");
-        }
-    }
-
-    // Handle object cannot be copied, but it can be moved!
+    // Handle object cannot be copied!
     Handle(const Handle&)            = delete;
     Handle& operator=(const Handle&) = delete;
 
-    Handle(Handle&& other) noexcept : handle_{other.handle_} { other.handle_ = nullptr; }
-    Handle& operator=(Handle&& other) noexcept {
-        handle_       = other.handle_;
-        other.handle_ = nullptr;
-        return *this;
-    };
+    ~Handle() = default;
 
-    ~Handle() { curl_easy_cleanup(handle_); }
+    Response perform(const URL& url, const Request<Method::GET>& request) {
+        // Set headers
+        const RequestHeader<Method::GET>& request_header = request.header();
+        set_headers(request_header.fields());
 
-    void set_headers(const List& headers) { curl_easy_setopt(handle_, CURLOPT_HTTPHEADER, headers.native()); }
+        // No body content
 
-    void set_url(const URL& url) {
-        auto url_as_string = url.str();
-        curl_easy_setopt(handle_, CURLOPT_URL, url_as_string.c_str());
+        // Make request
+        auto response = handle_.GET(url.str());
+
+        // Handle response
+        return to_response(response);
     }
 
-    void set_data(CURLoption option, const std::string& value) { curl_easy_setopt(handle_, option, &value); }
+    Response perform(const URL& url, const Request<Method::POST>& request) {
+        // Set headers
+        const RequestHeader<Method::POST>& request_header = request.header();
+        set_headers(request_header.fields());
 
-    void set_noprogress(bool flag = true) { set_option(CURLOPT_NOPROGRESS, flag ? 1L : 0L); }
-    void set_keepalive(bool flag = true) { set_option(CURLOPT_TCP_KEEPALIVE, flag ? 1L : 0L); }
-    void set_maxredirs(long maxredirs) { set_option(CURLOPT_MAXREDIRS, maxredirs); }
+        // Set body
+        const Body& request_body = request.body();
+        const auto& request_data = request_body.value();
 
-    void set_verifyhost(bool flag = true) { set_option(CURLOPT_SSL_VERIFYHOST, flag ? 1L : 0L); }
-    void set_verifypeer(bool flag = true) { set_option(CURLOPT_SSL_VERIFYPEER, flag ? 1L : 0L); }
-    void set_verifystatus(bool flag = true) { set_option(CURLOPT_SSL_VERIFYSTATUS, flag ? 1L : 0L); }
+        // Make request
+        auto response = handle_.POST(url.str(), request_data);
 
-    template <typename VALUE>
-    void set_option(CURLoption option, VALUE value) {
-        curl_easy_setopt(handle_, option, value);
+        // Handle response
+        return to_response(response);
     }
 
-    void perform() {
-        if (auto result = curl_easy_perform(handle_); result != CURLE_OK) {
-            throw UnsuccessfulOperation("curl_easy_perform operation failed: ", curl_easy_strerror(result));
-        }
-    }
+    Response perform(const URL& url, const Request<Method::PUT>& request) {
+        // Set headers
+        const RequestHeader<Method::PUT>& request_header = request.header();
+        set_headers(request_header.fields());
 
-    [[nodiscard]] long response_code() const {
-        long code;
-        curl_easy_getinfo(handle_, CURLINFO_RESPONSE_CODE, &code);
-        return code;
+        // Set body
+        const Body& request_body = request.body();
+        const auto& request_data = request_body.value();
+
+        // Make request
+
+        auto response = handle_.PUT(url.str(), request_data);
+
+        // Handle response
+        return to_response(response);
     }
 
 private:
-    CURL* handle_;
+    void set_headers(const Fields& fields) {
+        eckit::EasyCURLHeaders fieldx;
+        for (const auto& field : fields) {
+            fieldx[field.name] = field.value;
+        }
+
+        handle_.headers(fieldx);
+    }
+
+    void set_verifyhost(bool flag = true) { handle_.sslVerifyHost(flag); }
+    void set_verifypeer(bool flag = true) { handle_.sslVerifyPeer(flag); }
+
+    static Response to_response(const eckit::EasyCURLResponse& response) {
+        Fields fields;
+        for (const auto& entry : response.headers()) {
+            fields.insert(Field{entry.first, entry.second});
+        }
+        auto response_header = ResponseHeader(Status::from_value(response.code()), fields);
+        auto response_body   = Body{response.body()};
+
+        return Response{response_header, response_body};
+    }
+
+private:
+    eckit::EasyCURL handle_;
 };
 
-}  // namespace WrapperCURL
+template <Method METHOD>
+Response handle_request(const Host& host, const Request<METHOD>& request) {
+    detail::Handle curl;
+    auto url = detail::URL{host, request.header().target()};
+
+    return curl.perform(url, request);
+}
+
+}  // namespace detail
+
 
 Response TinyRESTClient::handle(const Host& host, const Request<Method::GET>& request) const {
-
-    WrapperCURL::List header_fields;
-    for (const auto& field : request.header().fields()) {
-        header_fields.append(field.name, field.value);
-    }
-
-    WrapperCURL::Handle curl =
-        WrapperCURL::Handle::make_handle(WrapperCURL::URL{host, request.header().target()}, header_fields);
-
-    // Configure GET related options
-
-    Fields fields;
-    curl.set_option(CURLOPT_HEADERFUNCTION, WrapperCURL::write_headers_callback);
-    curl.set_option(CURLOPT_HEADERDATA, &fields);
-
-    Body body;
-    curl.set_option(CURLOPT_WRITEFUNCTION, WrapperCURL::write_body_callback);
-    curl.set_option(CURLOPT_WRITEDATA, &body);
-
-    try {
-        curl.perform();
-    }
-    catch (WrapperCURL::UnsuccessfulOperation& e) {
-        std::cout << "ERROR: " << e.what() << std::endl;
-        return Response{Status::Code::BAD_REQUEST};  // TODO: must report proper error!
-    }
-
-    auto code   = curl.response_code();
-    auto header = ResponseHeader(Status::from_value(code), fields);
-
-    return Response{header, body};
+    return detail::handle_request(host, request);
 }
 
 Response TinyRESTClient::handle(const Host& host, const Request<Method::POST>& request) const {
-
-    WrapperCURL::List headers;
-    for (const auto& field : request.header().fields()) {
-        headers.append(field.name, field.value);
-    }
-
-    WrapperCURL::Handle curl =
-        WrapperCURL::Handle::make_handle(WrapperCURL::URL{host, request.header().target()}, headers);
-
-    // Configure POST related options
-
-    curl.set_option(CURLOPT_POST, 1L);
-    curl.set_option(CURLOPT_POSTFIELDS, request.body().value().c_str());
-
-    Fields fields;
-    curl.set_option(CURLOPT_HEADERFUNCTION, WrapperCURL::write_headers_callback);
-    curl.set_option(CURLOPT_HEADERDATA, &fields);
-
-    Body body;
-    curl.set_option(CURLOPT_WRITEFUNCTION, WrapperCURL::write_body_callback);
-    curl.set_option(CURLOPT_WRITEDATA, &body);
-
-    try {
-        curl.perform();
-    }
-    catch (WrapperCURL::UnsuccessfulOperation& e) {
-        std::cout << "ERROR: " << e.what() << std::endl;
-        return Response{Status::Code::BAD_REQUEST};  // TODO: must report proper error!
-    }
-
-    auto code   = curl.response_code();
-    auto header = ResponseHeader(Status::from_value(code), fields);
-
-    return Response{header, body};
+    return detail::handle_request(host, request);
 }
 
 Response TinyRESTClient::handle(const Host& host, const Request<Method::PUT>& request) const {
-
-    WrapperCURL::List headers;
-    for (const auto& field : request.header().fields()) {
-        headers.append(field.name, field.value);
-    }
-
-    WrapperCURL::Handle curl =
-        WrapperCURL::Handle::make_handle(WrapperCURL::URL{host, request.header().target()}, headers);
-
-    // Configure PUT related options
-
-    curl.set_option(CURLOPT_UPLOAD, 1L);
-
-    curl.set_option(CURLOPT_READFUNCTION, WrapperCURL::read_callback);
-    curl.set_data(CURLOPT_READDATA, request.body().value());
-    curl.set_option(CURLOPT_INFILESIZE_LARGE, request.body().value().size());
-
-    Fields fields;
-    curl.set_option(CURLOPT_HEADERFUNCTION, WrapperCURL::write_headers_callback);
-    curl.set_option(CURLOPT_HEADERDATA, &fields);
-
-    Body body;
-    curl.set_option(CURLOPT_WRITEFUNCTION, WrapperCURL::write_body_callback);
-    curl.set_option(CURLOPT_WRITEDATA, &body);
-
-    try {
-        curl.perform();
-    }
-    catch (WrapperCURL::UnsuccessfulOperation& e) {
-        std::cout << "ERROR: " << e.what() << std::endl;
-        return Response{Status::Code::BAD_REQUEST};  // TODO: must report proper error!
-    }
-
-    auto code   = curl.response_code();
-    auto header = ResponseHeader(Status::from_value(code), fields);
-
-    return Response{header, body};
-};
+    return detail::handle_request(host, request);
+}
 
 }  // namespace net
 
